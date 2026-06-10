63489973c29c6b9c4c9cd7dbfa6e4a6f10fea751
Joseph Doba <josephdoba@gmail.com>
2026-06-10 16:22:43 -0700

    june 10th

---
+++ June2026/june-10th.js
@@ -0,0 +1,5 @@
+const name = "Joseph"
+const myName = "Joseph"
+
+console.log(`hello my name is ${name}`) // Hmm, why is this depreciated?
+console.log(`hello my name is ${myName}`) // Ohh because "name" is depreciated, not string interpolation

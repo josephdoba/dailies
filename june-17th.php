4aae80cc74c3879f13ccd07914d556ec8a45aeef
Joseph Doba <josephdoba@gmail.com>
2026-06-17 16:59:46 -0700

    June 17th

--- june-17th.php
+++ june-17th.php
@@ -1,15 +1,3 @@
-<?php
-// then in terminal, gonna run php -S localhost:8000... ay it worked! lets add in some html
-echo "The classic banger of learning any language.. HELLO WORLD!"
+<?php 
+echo $_SERVER['HTTP_ACCEPT'];
 ?>
-
-
-<!DOCTYPE html>
-<html>
-    <head>
-        <title>My first PHP website</title>
-    </head>
-    <body>
-        <?php echo '<p>here it is again: HELLO WORLD!</p>'; ?>
-    </body>
-</html>

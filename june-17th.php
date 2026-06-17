47c64e875120e3f2db640dce249eda3c83cc759f
Joseph Doba <josephdoba@gmail.com>
2026-06-17 16:44:56 -0700

    june 17th

--- june-17th.php
+++ june-17th.php
@@ -1,5 +1,15 @@
 <?php
+// then in terminal, gonna run php -S localhost:8000... ay it worked! lets add in some html
 echo "The classic banger of learning any language.. HELLO WORLD!"
 ?>
 
-<!-- then in terminal, gonna run php -S localhost:8000... ay it worked! -->
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>My first PHP website</title>
+    </head>
+    <body>
+        <?php echo '<p>here it is again: HELLO WORLD!</p>'; ?>
+    </body>
+</html>

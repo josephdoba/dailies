177b15c6c9cc5c21c3a083b8810718899f86ea1d
Joseph Doba <josephdoba@gmail.com>
2026-06-22 17:09:01 -0700

    June 22nd

--- June2026/june-22nd.js
+++ June2026/june-22nd.js
@@ -1,7 +1,16 @@
-const slackFunction = () => {
-    console.log("lets start coding...")
-    wait(2500)
-    console.log("tomorrow")
+// lets review async/await properly..
+
+// define the wait
+
+const sleepTime = (millisec) => new Promise(resolve => setTimeout(resolve, millisec));
+
+const slackOff = async () => {
+    console.log("lets get to the thing...");
+
+    await sleepTime(2500) // pause for 2.5 seconds
+
+    console.log("tomorrow!")
+
 }
 
-slackFunction()
+slackOff()

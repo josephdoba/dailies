ed464cbc211634e6d97abe403051fc165c8c1028
Joseph Doba <josephdoba@gmail.com>
2026-07-06 17:30:35 -0700

    July 6th

--- July2026/july-6th.py
+++ July2026/july-6th.py
@@ -7,7 +7,12 @@ import asyncio # imports the async/await and allows for concurrent code
 # allows for coroutines, network io, subprocesses, task distribution, and sync's concurrent code.
 
 async def main():
-    await asyncio.sleep(1)
+    await asyncio.sleep(2)
     print("Hello!")
 
-asyncio.run(main())
+# asyncio.run(main()) 
+
+with asyncio.Runner() as runner:
+    runner.run(main())
+
+    # hm, whats the difference between this one and the runner one?

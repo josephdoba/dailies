94ca585737bb878a679cc74f38562158d46cd997
Joseph Doba <josephdoba@gmail.com>
2026-07-06 17:28:38 -0700

    July 6th

--- July2026/july-6th.py
+++ July2026/july-6th.py
@@ -1,1 +1,13 @@
 print("today we're going through the networking section in python")
+
+# https://docs.python.org/3/library/ipc.html
+
+import asyncio # imports the async/await and allows for concurrent code
+
+# allows for coroutines, network io, subprocesses, task distribution, and sync's concurrent code.
+
+async def main():
+    await asyncio.sleep(1)
+    print("Hello!")
+
+asyncio.run(main())

580bd8ae324a5be6e6411e8a68e7e4377cf99221
Joseph Doba <josephdoba@gmail.com>
2026-08-10 20:05:31 -0700

    aug 10th

--- Aug2026/aug-10th.php
+++ Aug2026/aug-10th.php
@@ -8,5 +8,6 @@
 
    //var_dump($myObject)  Ahh so this just, prints whichever var you assign into the console. Neat!
 
-    var_dump($myObject("meat")); // also very interesting you can only have one var dump.. perhaps its returning it? 
+    var_dump($myObject["fruit"]); // also very interesting you can only have one var dump.. perhaps its returning it? 
+    var_dump($myObject["meat"]); // hm, but then this statement isn't throwing an error.
 ?>

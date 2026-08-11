82ac264e2793207eb8eb3770d044483b9aebe90a
Joseph Doba <josephdoba@gmail.com>
2026-08-10 20:06:54 -0700

    aug 10th

--- Aug2026/aug-10th.php
+++ Aug2026/aug-10th.php
@@ -7,7 +7,7 @@
     );
 
    //var_dump($myObject)  Ahh so this just, prints whichever var you assign into the console. Neat!
-
+    var_dump($myObject);
     var_dump($myObject["fruit"]); // also very interesting you can only have one var dump.. perhaps its returning it? 
-    var_dump($myObject["meat"]); // hm, but then this statement isn't throwing an error.
+    var_dump($myObject["meat"]); // hm, but then this statement isn't throwing an error... Ah! user error. classic rookie mistake not closing statements. 
 ?>

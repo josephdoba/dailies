4913e2a2d9776190cb7ba92ccd9fd4a6e69b180e
Joseph Doba <josephdoba@gmail.com>
2026-08-18 15:42:52 -0700

    aug 17th git fix

---
+++ mergesort.php
@@ -0,0 +1,33 @@
+<!-- Also, I just thought of this algorithm while at work... its probably not revolutionary, but im a bit chuft I thought of it without cracking open google
+
+ArrayToSort = [4, 2, 7, 5, 3, 8, 9]
+
+ArrayDivide= 2
+
+Make arrays based on number of ArrayFraction, rounded up
+
+Sort each sub array of each element from lowest to highest, by checking the current item to the next element. If that element is higher than the iterated element,  push to end, else push it back one
+
+then amalgamate the correct order to a single array -->
+
+<?php
+
+    $array = [4, 2, 7, 5, 3, 8, 9];
+    function sorting($arraySort){
+        foreach ($arraySort as $i){
+            if ($i < 5){
+                echo "{$i} is less than 5\n";
+            } else {
+                echo "{$i} is greater or equal to 5\n";
+            }
+        }
+
+
+    }
+
+    
+    sorting($array)
+
+
+
+?>
